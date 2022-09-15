460ae6b77ab8ea5ae874e7d2d37532135f4f455e
Pranta Protik <pranta.cseku@gmail.com>
2022-09-15 12:25:23 +0600

    Sphere and torus function added

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -11,27 +11,14 @@ public class Graph : MonoBehaviour
     private void Awake()
     {
         float step = 2f / _resolution;
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
 
         _points = new Transform[_resolution * _resolution];
 
-        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
+        for (int i = 0; i < _points.Length; i++)
         {
-            if (x == _resolution)
-            {
-                x = 0;
-                z += 1;
-            }
-
             Transform point = _points[i] = Instantiate(_pointPrefab);
-
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            
-            point.localPosition = position;
             point.localScale = scale;
-
             point.SetParent(transform, false);
         }
     }
@@ -41,12 +28,21 @@ public class Graph : MonoBehaviour
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(_function);
 
         float time = Time.time;
+        float step = 2f / _resolution;
 
-        foreach (var point in _points)
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
         {
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+            if (x == _resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+
+            _points[i].localPosition = f(u, v, time);
         }
     }
 }
